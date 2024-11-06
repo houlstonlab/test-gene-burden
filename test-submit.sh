a1331378a7653641e3f5a9614946e54f5419f522
mahmed03 <mahmoud.ahmed@icr.ac.uk>
2024-11-06 11:11:04 +0000

    add gha

--- test-submit.sh
+++ test-submit.sh
@@ -7,22 +7,25 @@
 #SBATCH -t 120:00:00
 
 # Setup test directory
-mkdir -p test/
+mkdir -p test/ test/input
 cd test/
 
-# Load required modules
-module load Nextflow/24.04.2
+# Download test data
+URL="https://raw.githubusercontent.com/houlstonlab/toy-datasets/refs/heads/main/cohort-counts/"
+for file in cohorts_info.csv general.categoryA.aggregate.tsv general.categoryB.aggregate.tsv pheno.categoryA.aggregate.tsv pheno.categoryB.aggregate.tsv; do
+    wget -O input/$file $URL/$file
+done
 
 # Run nextflow
-# nextflow run houlstonlab/test-gene-burden -r main \
+# nextflow run houlstonlab/test-gene-burden -r test-gh \
 nextflow run ../main.nf \
     --output_dir ./results/ \
-    -profile local,test \
+    -profile local,gha \
     -resume
 
 # usage: nextflow run [ local_dir/main.nf | git_url ]  
 # These are the required arguments:
-#     -r            {main,dev} to run specific branch
+#     -r            {main,dev,gha} to run specific branch
 #     -profile      {local,cluster} to run using differens resources
 #     -params-file  params.json to pass parameters to the pipeline
 #     -resume       To resume the pipeline from the last checkpoint
